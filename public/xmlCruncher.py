ad73cd9b66ba4261c376972348924600ace9758c
IhsanE <ihsanetwaroo@gmail.com>
2014-03-20 00:50:07 -0400

    python cruncher complete

--- public/xmlCruncher.py
+++ public/xmlCruncher.py
@@ -1,4 +1,25 @@
+"""
+When passing in the url as an argument, note they are indexed by & symbols b/c
+you cannot send a command line argument with the & symbol.
+"""
+
+from xml.dom import minidom
+from os import sys
 import urllib.request
-page = urllib.request.urlopen('https://api.meetup.com/2/open_events.xml?topic=photo&key=346f4a513547d677934427478787649')
+print((sys.argv))
+url = ''
+for i in sys.argv[1:]:
+    url+=i+'&'
+url = url[:-1]
+print(url)
+page = urllib.request.urlopen(url)
 f = open('xmlFile.xml', 'w')
 a = str(page.read())
+f.write(a[46:-3])
+f.close()
+xmldoc = minidom.parse('xmlFile.xml')
+itemlist = xmldoc.getElementsByTagName('item') 
+print (len(itemlist))
+#print (itemlist[0].attributes['name'].value)
+for s in itemlist :
+    print (s.firstChild.firstChild.firstChild)
