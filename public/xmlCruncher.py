3a68b5d00cc1ccce93b9659386e5659f112a0f8b
IhsanE <ihsanetwaroo@gmail.com>
2014-03-21 12:18:51 -0400

    Fixed url link

--- public/xmlCruncher.py
+++ public/xmlCruncher.py
@@ -8,7 +8,7 @@ from os import sys
 import urllib.requeprint((sys.argv))
 fi = open('meetupData.txt', 'r')
 url = ''
-url = fi.readline()[:-2]
+url = "https://api.meetup.com/2/open_events.xml?topic="+fi.readline()[:-2]+"&time=,1w&key=346f4a513547d677934427478787649"
 page = urllib.request.urlopen(url)
 f = open('xmlFile.xml', 'w')
 o = open('meetupData.txt', 'w')
